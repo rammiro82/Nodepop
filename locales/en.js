c0dc0a1d578a26c6b7b6427e5b90aa45ee0fb21d
Ramiro García Salazar <rammiro82@gmail.com>
2015-10-05 00:16:07 +0200

    Traducir el fichero del locale del inglés

--- locales/en.js
+++ locales/en.js
@@ -1,10 +1,10 @@
 {
 	"DB_CONN_OK": "Conection to DDBB OK.",
-	"USR_POST_AUTH_KO": "Fallo al realizar el post del usuarios. ",
-	"USR_POST_AUTH_KO_DUPLICADO": "Email ya registrado. ",
-	"USR_POST_AUTH_KO_EMAIL_VACIO": "USR_POST_AUTH_KO_EMAIL_VACIO",
-	"ADS_GET_PARAM_NO_VALIDO": "Not valid param %s.",
-	"ADS_GET_PARAM_NO_VALIDO_CORTO": "Parámetro %s no válido, al menos tienes que tener %d caracteres.",
-	"ADS_GET_PARAM_NO_VALIDO_ARRAY": "ADS_GET_PARAM_NO_VALIDO_ARRAY %s %s",
-	"TOKEN_SAVE_KO": "TOKEN_SAVE_KO"
+    "USR_POST_AUTH_KO": "User post failed. ",
+    "USR_POST_AUTH_KO_DUPLICADO": "Email allready registred. ",
+    "USR_POST_AUTH_KO_EMAIL_VACIO": "Empty email field.",
+    "ADS_GET_PARAM_NO_VALIDO": "%s, not valid param.",
+    "ADS_GET_PARAM_NO_VALIDO_CORTO": "%s, not valid param, %d chars required.",
+    "ADS_GET_PARAM_NO_VALIDO_ARRAY": "%s param not valid, %s possible values."
+    "TOKEN_SAVE_KO": "Error saving PushToken"
 }
--- locales/es.js
+++ locales/es.js
@@ -2,8 +2,9 @@
 	"DB_CONN_OK": "Conexión establecida con la BBDD correctamente. ",
 	"USR_POST_AUTH_KO": "Fallo al realizar el post del usuarios. ",
 	"USR_POST_AUTH_KO_DUPLICADO": "Email ya registrado. ",
-	"USR_POST_AUTH_KO_EMAIL_VACIO": "USR_POST_AUTH_KO_EMAIL_VACIO",
+	"USR_POST_AUTH_KO_EMAIL_VACIO": "Parámetro email, vacío",
 	"ADS_GET_PARAM_NO_VALIDO": "El parámetro %s no es válido",
 	"ADS_GET_PARAM_NO_VALIDO_CORTO": "Parámetro %s no válido, al menos tienes que tener %d caracteres.",
-	"ADS_GET_PARAM_NO_VALIDO_ARRAY": "ADS_GET_PARAM_NO_VALIDO_ARRAY %s %s"
+	"ADS_GET_PARAM_NO_VALIDO_ARRAY": "Valor del parámetro %s no válido, los posibles valores son %s."
+    "TOKEN_SAVE_KO": "Error al guardar el PushToken"
 }
