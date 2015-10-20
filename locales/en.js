1d94aaef2095f7e9677be9eeb565083cfd14fc01
Ramiro García Salazar <rammiro82@gmail.com>
2015-10-20 23:12:00 +0200

    Corrigiendo los ficheros i18n para que carguen correctamente

--- locales/en.js
+++ locales/en.js
@@ -5,6 +5,6 @@
     "USR_POST_AUTH_KO_EMAIL_VACIO": "Empty email field.",
     "ADS_GET_PARAM_NO_VALIDO": "%s, not valid param.",
     "ADS_GET_PARAM_NO_VALIDO_CORTO": "%s, not valid param, %d chars required.",
-    "ADS_GET_PARAM_NO_VALIDO_ARRAY": "%s param not valid, %s possible values."
+    "ADS_GET_PARAM_NO_VALIDO_ARRAY": "%s param not valid, %s possible values.",
     "TOKEN_SAVE_KO": "Error saving PushToken"
 }
--- locales/es.js
+++ locales/es.js
@@ -5,6 +5,6 @@
 	"USR_POST_AUTH_KO_EMAIL_VACIO": "Parámetro email, vacío",
 	"ADS_GET_PARAM_NO_VALIDO": "El parámetro %s no es válido",
 	"ADS_GET_PARAM_NO_VALIDO_CORTO": "Parámetro %s no válido, al menos tienes que tener %d caracteres.",
-	"ADS_GET_PARAM_NO_VALIDO_ARRAY": "Valor del parámetro %s no válido, los posibles valores son %s."
+	"ADS_GET_PARAM_NO_VALIDO_ARRAY": "Valor del parámetro %s no válido, los posibles valores son %s.",
     "TOKEN_SAVE_KO": "Error al guardar el PushToken"
 }
